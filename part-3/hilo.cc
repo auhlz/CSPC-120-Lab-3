e286682599545292e1260149001ddbf97d20de21
Alex Labitigan <alexlabitigan@csu.fullerton.edu>
2022-09-22 23:24:40 -0700

    fixed formatting and finished lab 3 part 3

--- part-3/hilo.cc
+++ part-3/hilo.cc
@@ -20,14 +20,13 @@ int main(int argc, char const *argv[]) {
   std::cin >> guess1;
 
   if (guess1 > secret_number) {
-    std::cout << "Too high";
+    std::cout << "Too high\n";
   } else if (guess1 < secret_number) {
     std::cout << "Too low\n";
-  }  else if (guess1 == secret_number) {
+  } else if (guess1 == secret_number) {
     std::cout << "Correct, you win!\n";
     return 0;
   }
-  
   int guess2;
   std::cout << "Player 2, enter your second guess: ";
   std::cin >> guess2;
@@ -35,10 +34,11 @@ int main(int argc, char const *argv[]) {
     std::cout << "Too high" << std::endl;
   } else if (guess2 < secret_number) {
     std::cout << "Too low\n";
-  }  else if (guess2 == secret_number) {
+  } else if (guess2 == secret_number) {
     std::cout << "Correct, you win!";
     return 0;
   }
-std::cout << "You Lose!\n";
+  std::cout << "Incorrect the secret number was " << secret_number
+            << ", you lose!\n";
   return 0;
 }
